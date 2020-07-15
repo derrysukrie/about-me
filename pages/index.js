cdcc608429503b2269ac838eb2580f2c76effe78
derrysukrie <derrysukrie@gmail.com>
2020-07-15 21:54:26 +0700

    fixed head

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,32 @@
-import Navbar from 'src/components/Navbar/Navbar'
-import DefaultLayout from 'src/layout/DefaultLayout'
-import Aboutme from 'src/components/Aboutme/Aboutme';
-import './app.scss';
-import Techstack from 'src/components/Techstack/Techstack';
+import Head from "next/head";
+
+import Navbar from "src/components/Navbar/Navbar";
+import DefaultLayout from "src/layout/DefaultLayout";
+import Aboutme from "src/components/Aboutme/Aboutme";
+import "./app.scss";
+import Techstack from "src/components/Techstack/Techstack";
 
 export default function Home() {
   return (
-    <DefaultLayout>
-      <Navbar />
-      <Aboutme />
-      <Techstack />
-    </DefaultLayout>
+    <>
+      <Head>
+        <title>Derry Sukrie</title>
+        <link rel='icon' href='/favicon.ico' />
+        <html lang='en' />
+        <meta
+          name='description'
+          content='A Frontend Engineer based in Jakarta, Indonesia'
+        />
+        <link
+          href='https://fonts.googleapis.com/css2?family=Barlow:wght@300;400;500;600;700&display=swap'
+          rel='stylesheet'
+        />
+      </Head>
+      <DefaultLayout>
+        <Navbar />
+        <Aboutme />
+        <Techstack />
+      </DefaultLayout>
+    </>
   );
-};
-
+}
--- src/layout/DefaultLayout.js
+++ src/layout/DefaultLayout.js
@@ -1,18 +1,11 @@
 import React from 'react';
-import Head from 'next/head'
 
 const DefaultLayout = ({ children }) => {
   return (
     <div className="container">
-      <Head>
-        <title>Derry Sukrie</title>
-        <link rel="icon" href="/favicon.ico" />
-        <html lang="en" />
-        <meta name="description" content="A Frontend Engineer based in Jakarta, Indonesia"/>
-        <link href="https://fonts.googleapis.com/css2?family=Barlow:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
-        <meta property="og:title" content="My new title" key="title" />
-      </Head>
-      {children}
+      <main>
+        {children}
+      </main>
       <style jsx>{`
         .container {
           box-sizing: border-box;
