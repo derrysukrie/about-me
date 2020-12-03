e84f01a91949bb1ccd90ede467319a2d410d39f7
derrysukrie <derrysukrie@gmail.com>
2020-12-03 12:08:12 +0700

    saperated _app and _document

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,16 @@
-import app from './_app';
+import React from 'react';
 
-export default app;
+import DefaultLayout from 'layout/DefaultLayout';
+import Navbar from 'components/Navbar/Navbar';
+import Aboutme from 'components/Aboutme/Aboutme';
+import Techstack from 'components/Techstack/Techstack';
+
+export default function Home() {
+  return (
+    <DefaultLayout>
+      <Navbar />
+      <Aboutme />
+      <Techstack />
+    </DefaultLayout>
+  );
+}
