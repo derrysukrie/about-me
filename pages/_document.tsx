01fa6936f692395dd127372a1dcb9fd5ace54559
derrysukrie <derrysukrie@gmail.com>
2021-02-04 11:09:17 +0700

    remove index

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,10 +9,13 @@ export default class CustomDocument extends Document {
           <link rel="icon" href="/favicon.ico" />
           <meta
             name="description"
-            content="A Frontend Engineer based in Jakarta, Indonesia"
+            content="Frontend Engineer based in Jakarta, Indonesia"
           />
-          <meta property="og:image"  content="/favicon.ico" />
-          <meta name="keywords" content="HTML, CSS, JavaScript, React, NextJS, Redux" />
+          <meta property="og:image" content="/favicon.ico" />
+          <meta
+            name="keywords"
+            content="HTML, CSS, JavaScript, React, NextJS, Redux"
+          />
           <meta name="author" content="Derry Sukrie" />
           <link
             href="https://fonts.googleapis.com/css2?family=Barlow:wght@300;400;500;600;700&display=swap"
